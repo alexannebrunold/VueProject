f43a5ebd597aa057d2997ea3d0ed459e3d16f146
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-05-30 17:07:18 +0200

    init

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from "vue"
+import Router from "vue-router"
+import EventCreate from "../views/EventCreate.vue"
+import EventList from "../views/EventList.vue"
+import EventShow from "../views/EventShow.vue"
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "event-list",
+      component: EventList
+    },
+    {
+      path: "/event",
+      name: "event-show",
+      component: EventShow
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      //   component: () =>
+      //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+    },
+    {
+      path: "/event/create",
+      name: "event-create",
+      component: EventCreate
+    }
+  ]
+})
+
+// const router = new VueRouter({
+//   routes
+// })
+
+// export default router
