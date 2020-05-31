876f30f511237ef10bb9906f42d4e51a1c53a297
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-05-31 08:55:07 +0200

    call axios

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :height="height">
+      <use v-bind="{ 'xlink:href': '/users.svg#' + name }" />
+    </svg>
+    <slot> </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
+    <EventCard
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    ></EventCard>
     <router-link
       :to="{ name: 'event-show', params: { id: '1' } }"
     ></router-link>
+    <BaseIcon />
   </div>
 </template>
 
 <script>
 import EventCard from "@/components/EventCard.vue"
+import EventService from "@/services/EventService.js"
+
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data // For now, logs out the response
+      })
+      .catch(error => {
+        console.log("There was an error:" + error.response) // Logs out the error
+      })
   }
 }
 </script>
